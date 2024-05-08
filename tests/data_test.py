640aaf8843861a1f3a0a9ee82748c031193f78e6
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-05-08 16:54:58 -0500

    Add randbytes utility function

---
+++ tests/data_test.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import pytest
+
+from webs.data import randbytes
+
+
+@pytest.mark.parametrize('size', (0, 1, 10, 100))
+def test_randbytes(size: int) -> None:
+    b = randbytes(size)
+    assert isinstance(b, bytes)
+    assert len(b) == size
