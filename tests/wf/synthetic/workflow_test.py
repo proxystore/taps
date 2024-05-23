f1b2dd6a5f8a08f6b645323e138e04cfc6236d6a
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-05-22 23:17:43 -0500

    Expand synthetic workflow

--- tests/wf/synthetic/workflow_test.py
+++ tests/wf/synthetic/workflow_test.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 
+import math
 import pathlib
 import time
+from unittest import mock
 
 from webs.executor.workflow import WorkflowExecutor
 from webs.wf.synthetic.config import SyntheticWorkflowConfig
+from webs.wf.synthetic.config import WorkflowStructure
 from webs.wf.synthetic.workflow import noop_task
+from webs.wf.synthetic.workflow import run_bag_of_tasks
+from webs.wf.synthetic.workflow import run_diamond
+from webs.wf.synthetic.workflow import run_reduce
+from webs.wf.synthetic.workflow import run_sequential
 from webs.wf.synthetic.workflow import SyntheticWorkflow
 
 
@@ -14,26 +21,88 @@ def test_noop_task() -> None:
     sleep = 0.001
 
     start = time.perf_counter()
-    result = noop_task(b'data', output_size, sleep)
+    result = noop_task(b'data', output_size=output_size, sleep=sleep)
     runtime = time.perf_counter() - start
 
     assert runtime >= sleep
     assert len(result) == output_size
 
 
-def test_run_synthetic_workflow(
+def test_synthetic_workflow(
     workflow_executor: WorkflowExecutor,
     tmp_path: pathlib.Path,
 ) -> None:
     config = SyntheticWorkflowConfig(
+        structure=WorkflowStructure.BAG,
         task_count=3,
         task_data_bytes=100,
         task_sleep=0.001,
+        bag_max_running=3,
     )
-    workflow = SyntheticWorkflow.from_config(config)
+
+    kinds = {
+        WorkflowStructure.BAG: run_bag_of_tasks,
+        WorkflowStructure.DIAMOND: run_diamond,
+        WorkflowStructure.REDUCE: run_reduce,
+        WorkflowStructure.SEQUENTIAL: run_sequential,
+    }
+
+    for kind, function in kinds.items():
+        config.structure = kind
+        workflow = SyntheticWorkflow.from_config(config)
+
+        with mock.patch(
+            f'webs.wf.synthetic.workflow.{function.__name__}',
+        ) as mocked:
+            workflow.run(workflow_executor, tmp_path)
+            mocked.assert_called_once()
+
+
+def test_run_bag_of_tasks(workflow_executor: WorkflowExecutor) -> None:
+    task_count, task_sleep, max_running_tasks = 6, 0.001, 3
 
     start = time.perf_counter()
-    workflow.run(workflow_executor, tmp_path)
+    run_bag_of_tasks(
+        workflow_executor,
+        task_count,
+        0,
+        task_sleep,
+        max_running_tasks,
+    )
     runtime = time.perf_counter() - start
 
-    assert runtime >= config.task_count * config.task_sleep
+    min_time = math.ceil(task_count / max_running_tasks) * task_sleep
+    max_time = task_sleep * task_count
+    assert min_time <= runtime <= max_time
+
+
+def test_run_diamond(workflow_executor: WorkflowExecutor) -> None:
+    task_count, task_sleep = 3, 0.001
+
+    start = time.perf_counter()
+    run_diamond(workflow_executor, task_count, 0, task_sleep)
+    runtime = time.perf_counter() - start
+
+    layers = 3
+    assert layers * task_sleep <= runtime <= (task_count + 2) * task_sleep
+
+
+def test_run_reduce(workflow_executor: WorkflowExecutor) -> None:
+    task_count, task_sleep = 3, 0.001
+
+    start = time.perf_counter()
+    run_reduce(workflow_executor, task_count, 0, task_sleep)
+    runtime = time.perf_counter() - start
+
+    layers = 2
+    assert layers * task_sleep <= runtime <= (task_count + 1) * task_sleep
+
+
+def test_run_sequential(workflow_executor: WorkflowExecutor) -> None:
+    task_count, task_sleep = 3, 0.001
+
+    start = time.perf_counter()
+    run_sequential(workflow_executor, task_count, 0, task_sleep)
+    runtime = time.perf_counter() - start
+
+    assert runtime >= task_count * task_sleep
--- webs/wf/synthetic/config.py
+++ webs/wf/synthetic/config.py
@@ -1,13 +1,28 @@
 from __future__ import annotations
 
+import enum
+
 from pydantic import Field
 
 from webs.config import Config
 
 
+class WorkflowStructure(enum.Enum):
+    """Workflow structure types."""
+
+    BAG = 'bag'
+    DIAMOND = 'diamond'
+    REDUCE = 'reduce'
+    SEQUENTIAL = 'sequential'
+
+
 class SyntheticWorkflowConfig(Config):
     """Synthetic workflow configuration."""
 
+    structure: WorkflowStructure = Field(description='workflow structure')
     task_count: int = Field(description='number of tasks in the workflow')
     task_data_bytes: int = Field(description='intermediate task data size')
     task_sleep: float = Field(description='minimum duration of each task')
+    bag_max_running: int = Field(
+        description='max running tasks in bag workflow',
+    )
