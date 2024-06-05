f9878bc406586b580177085a6a9149f8ed954599
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-06-05 02:38:30 +0000

    Refactor montage app and update docs

--- taps/run/apps/montage.py
+++ taps/run/apps/montage.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import pathlib
+
 from pydantic import Field
+from pydantic import field_validator
 
 from taps.app import App
 from taps.app import AppConfig
@@ -12,16 +15,33 @@ class MontageConfig(AppConfig):
     """Montage application configuration."""
 
     img_folder: str = Field(description='input images folder path')
-    img_tbl: str = Field(description='input image table file')
-    img_hdr: str = Field(description='header filename for input images')
-    output_dir: str = Field(description='output folder path')
+    img_tbl: str = Field(
+        'Kimages.tbl',
+        description='input image table filename',
+    )
+    img_hdr: str = Field(
+        'Kimages.hdr',
+        description='header filename for input images',
+    )
+    output_dir: str = Field(
+        'data',
+        description=(
+            'output folder path for intermediate and final data '
+            '(relative to run directory)'
+        ),
+    )
+
+    @field_validator('img_folder', mode='before')
+    @classmethod
+    def _resolve_paths(cls, root: str) -> str:
+        return str(pathlib.Path(root).resolve())
 
     def create_app(self) -> App:
         """Create an application instance from the config."""
         from taps.apps.montage import MontageApp
 
         return MontageApp(
-            img_folder=self.img_folder,
+            img_folder=pathlib.Path(self.img_folder),
             img_tbl=self.img_tbl,
             img_hdr=self.img_hdr,
             output_dir=self.output_dir,
