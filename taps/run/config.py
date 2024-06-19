e53a10d157facb4809742a565e526ba9fdbc9997
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-06-19 12:21:21 -0500

    Refactor config settings sources

--- taps/run/config.py
+++ taps/run/config.py
@@ -18,9 +18,6 @@ from pydantic import create_model
 from pydantic import Field
 from pydantic import model_validator
 from pydantic_settings import BaseSettings
-from pydantic_settings import CliSettingsSource
-from pydantic_settings import PydanticBaseSettingsSource
-from pydantic_settings import TomlConfigSettingsSource
 
 from taps.app import App
 from taps.engine import AppEngineConfig
@@ -110,34 +107,10 @@ class Config(BaseSettings):
         output: Output configuration.
     """
 
-    config: Optional[pathlib.Path] = Field(None)  # noqa: UP007
     app: AppConfigs
     engine: AppEngineConfig = Field(default_factory=AppEngineConfig)
     logging: LoggingConfig = Field(default_factory=LoggingConfig)
     output: OutputConfig = Field(default_factory=OutputConfig)
-
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        env_settings: PydanticBaseSettingsSource,
-        dotenv_settings: PydanticBaseSettingsSource,
-        file_secret_settings: PydanticBaseSettingsSource,
-    ) -> tuple[PydanticBaseSettingsSource, PydanticBaseSettingsSource]:
-        toml_file: str | None = None
-        for i, arg in enumerate(sys.argv):
-            if not arg.startswith('--config'):
-                continue
-            if arg.startswith('--config='):
-                toml_file = arg.split('=')[1]
-            else:
-                toml_file = sys.argv[i + 1]
-
-        return (
-            CliSettingsSource(settings_cls, cli_parse_args=True),
-            TomlConfigSettingsSource(settings_cls, toml_file=toml_file),
-        )
 
     def write_toml(self, filepath: str | pathlib.Path) -> None:
         model = self.model_dump(exclude_none=True)
--- taps/run/main.py
+++ taps/run/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import argparse
 import contextlib
 import functools
 import logging
@@ -7,8 +8,17 @@ import os
 import pathlib
 import sys
 import time
+from collections.abc import MutableMapping
+from typing import Any
 from typing import Callable
 from typing import Sequence
+
+if sys.version_info >= (3, 11):  # pragma: >=3.11 cover
+    import tomllib
+else:  # pragma: <3.11 cover
+    import tomli as tomllib
+
+from pydantic_settings import CliSettingsSource
 
 from taps.logging import init_logging
 from taps.logging import RUN_LOG_LEVEL
@@ -16,6 +26,41 @@ from taps.run.config import Config
 from taps.run.config import make_run_dir
 
 logger = logging.getLogger('taps.run')
+
+
+def _flatten_dict(
+    d: MutableMapping[str, Any],
+    parent_key: str = '',
+    separator: str = '.',
+) -> dict[str, Any]:
+    items: list[tuple[str, Any]] = []
+    for key, value in d.items():
+        new_key = parent_key + separator + key if parent_key else key
+        if isinstance(value, MutableMapping):
+            items.extend(
+                _flatten_dict(value, new_key, separator=separator).items(),
+            )
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+
+def _parse_args(
+    parser: argparse.ArgumentParser,
+    args: Sequence[str],
+) -> argparse.Namespace:
+    cli_args = parser.parse_args(args)
+
+    if cli_args.config is not None:
+        with open(cli_args.config, 'rb') as f:
+            config_options = tomllib.load(f)
+        toml_args = argparse.Namespace(**_flatten_dict(config_options))
+    else:
+        toml_args = argparse.Namespace()
+
+    # cli_args takes precedence when the same key is present in both.
+    final_args = {**vars(toml_args), **vars(cli_args)}
+    return argparse.Namespace(**final_args)
 
 
 def parse_args_to_config(argv: Sequence[str]) -> Config:
@@ -27,7 +72,28 @@ def parse_args_to_config(argv: Sequence[str]) -> Config:
     Returns:
         Configuration.
     """
-    return Config()
+    parser = argparse.ArgumentParser(
+        description='Task Performance Suite.',
+        prog='python -m taps.run',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        '-c',
+        '--config',
+        help='toml configuration file to load',
+    )
+
+    cli_settings = CliSettingsSource(
+        Config,
+        cli_avoid_json=True,
+        cli_parse_args=argv,
+        cli_parse_none_str='null',
+        root_parser=parser,
+        parse_args_method=_parse_args,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    return Config(_cli_settings_source=cli_settings)
 
 
 def _cwd_run_dir(
