a94975551fed1a2d6cadfc0eeb8d1e7cf58a900a
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-07-12 18:29:06 -0500

    Migrate ParslConfig to ParslLocalConfig
    This is in anticipation of adding a more full featured ParslConfig
    according to #79. However, the full featured config will be more complex
    to configure so I think it makes sense to have a simple parsl-local
    executor that just uses an HTEX on the local node.

--- taps/executor/parsl.py
+++ taps/executor/parsl.py
@@ -4,13 +4,11 @@ import multiprocessing
 from typing import Literal
 from typing import Optional
 
-import globus_compute_sdk
 from parsl.addresses import address_by_hostname
 from parsl.channels import LocalChannel
 from parsl.concurrent import ParslPoolExecutor
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
-from parsl.executors import ThreadPoolExecutor
 from parsl.providers import LocalProvider
 from pydantic import Field
 
@@ -19,56 +17,42 @@ from taps.plugins import register
 
 
 @register('executor')
-class ParslConfig(ExecutorConfig):
-    """`ParslPoolExecutor` plugin configuration.
+class ParslLocalConfig(ExecutorConfig):
+    """Local `ParslPoolExecutor` plugin configuration.
+
+    Simple Parsl configuration that uses the
+    [`HighThroughputExecutor`][parsl.executors.HighThroughputExecutor]
+    on the local node.
 
     Attributes:
-        use_threads: Use Parsl's
-            [`ThreadPoolExecutor`][parsl.executors.ThreadPoolExecutor]
-            rather than
-            [`HighThroughputExecutor][parsl.executors.HighThroughputExecutor].
         workers: Maximum number of Parsl workers.
         run_dir: Parsl run directory.
     """
 
-    name: Literal['parsl'] = 'parsl'
-    use_threads: bool = Field(
-        False,
-        description=(
-            'use parsl ThreadPoolExecutor instead of HighThroughputExecutor'
-        ),
-    )
+    name: Literal['parsl-local'] = 'parsl-local'
     workers: Optional[int] = Field(None, description='max parsl workers')  # noqa: UP007
     run_dir: str = Field(
         'parsl-runinfo',
         description='parsl run directory within the app run directory',
     )
 
-    def get_executor_config(self) -> Config:
-        """Create a Parsl config from this config."""
+    def get_executor(self) -> ParslPoolExecutor:
+        """Create an executor instance from the config."""
         workers = (
             self.workers
             if self.workers is not None
             else multiprocessing.cpu_count()
         )
-
-        if self.use_threads:
-            executor = ThreadPoolExecutor(max_threads=workers)
-        else:
-            executor = HighThroughputExecutor(
-                label='htex-local',
-                max_workers_per_node=workers,
-                address=address_by_hostname(),
-                cores_per_worker=1,
-                provider=LocalProvider(
-                    channel=LocalChannel(),
-                    init_blocks=1,
-                    max_blocks=1,
-                ),
-            )
-
-        return Config(executors=[executor], run_dir=self.run_dir)
-
-    def get_executor(self) -> globus_compute_sdk.Executor:
-        """Create an executor instance from the config."""
-        return ParslPoolExecutor(self.get_executor_config())
+        executor = HighThroughputExecutor(
+            label='htex-local',
+            max_workers_per_node=workers,
+            address=address_by_hostname(),
+            cores_per_worker=1,
+            provider=LocalProvider(
+                channel=LocalChannel(),
+                init_blocks=1,
+                max_blocks=1,
+            ),
+        )
+        config = Config(executors=[executor], run_dir=self.run_dir)
+        return ParslPoolExecutor(config)
--- tests/executor/parsl_test.py
+++ tests/executor/parsl_test.py
@@ -3,23 +3,11 @@ from __future__ import annotations
 import pathlib
 from concurrent.futures import Executor
 
-from taps.executor.parsl import ParslConfig
+from taps.executor.parsl import ParslLocalConfig
 
 
-def test_get_thread_config(tmp_path: pathlib.Path) -> None:
+def test_get_local_executor(tmp_path: pathlib.Path) -> None:
     run_dir = str(tmp_path / 'parsl')
-    config = ParslConfig(use_threads=True, run_dir=run_dir)
-    config.get_executor_config()
-
-
-def test_get_process_config(tmp_path: pathlib.Path) -> None:
-    run_dir = str(tmp_path / 'parsl')
-    config = ParslConfig(use_threads=False, run_dir=run_dir)
-    config.get_executor_config()
-
-
-def test_get_executor(tmp_path: pathlib.Path) -> None:
-    run_dir = str(tmp_path / 'parsl')
-    config = ParslConfig(use_threads=True, run_dir=run_dir)
+    config = ParslLocalConfig(run_dir=run_dir)
     executor = config.get_executor()
     assert isinstance(executor, Executor)
