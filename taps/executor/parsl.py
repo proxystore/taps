fbe536e82ee448d4cc95c42c7cf57cbf659d5626
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-07-15 17:56:49 -0500

    Add the parsl-htex executor (#79)

--- taps/executor/parsl.py
+++ taps/executor/parsl.py
@@ -3,21 +3,29 @@ from __future__ import annotations
 import multiprocessing
 from typing import Literal
 from typing import Optional
+from typing import Tuple
+from typing import Union
 
+import parsl
 from parsl.addresses import address_by_hostname
 from parsl.channels import LocalChannel
 from parsl.concurrent import ParslPoolExecutor
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
+from parsl.launchers.base import Launcher
 from parsl.providers import LocalProvider
+from parsl.providers.base import ExecutionProvider
+from pydantic import BaseModel
+from pydantic import ConfigDict
 from pydantic import Field
+from pydantic import field_validator
 
-from taps.executor import ExecutorConfig
+from taps.executor import ExecutorConfig as TapsExecutorConfig
 from taps.plugins import register
 
 
 @register('executor')
-class ParslLocalConfig(ExecutorConfig):
+class ParslLocalConfig(TapsExecutorConfig):
     """Local `ParslPoolExecutor` plugin configuration.
 
     Simple Parsl configuration that uses the
@@ -30,7 +38,10 @@ class ParslLocalConfig(ExecutorConfig):
     """
 
     name: Literal['parsl-local'] = 'parsl-local'
-    workers: Optional[int] = Field(None, description='max parsl workers')  # noqa: UP007
+    workers: Optional[int] = Field(  # noqa: UP007
+        None,
+        description='max parsl workers',
+    )
     run_dir: str = Field(
         'parsl-runinfo',
         description='parsl run directory within the app run directory',
@@ -44,7 +55,7 @@ class ParslLocalConfig(ExecutorConfig):
             else multiprocessing.cpu_count()
         )
         executor = HighThroughputExecutor(
-            label='htex-local',
+            label='taps-htex-local',
             max_workers_per_node=workers,
             address=address_by_hostname(),
             cores_per_worker=1,
@@ -54,5 +65,295 @@ class ParslLocalConfig(ExecutorConfig):
                 max_blocks=1,
             ),
         )
-        config = Config(executors=[executor], run_dir=self.run_dir)
+        config = Config(
+            executors=[executor],
+            run_dir=self.run_dir,
+            initialize_logging=False,
+        )
         return ParslPoolExecutor(config)
+
+
+@register('executor')
+class ParslHTExConfig(TapsExecutorConfig):
+    """HTEx `ParslPoolExecutor` plugin configuration.
+
+    These parameters are a subset of
+    [`parsl.config.Config`][parsl.config.Config].
+
+    For simple, single-node HTEx deployments, prefer
+    [`ParslLocalConfig`][taps.executor.parsl.ParslLocalConfig].
+
+    Attributes:
+        htex: HTEx configuration.
+        app_cache: Enable app caching.
+        retries: Number of retries in case of task failure.
+        strategy: Block scaling strategy.
+        max_idletime: Max idle time before strategy can shutdown unused blocks.
+        run_dir: Parsl run directory.
+    """
+
+    name: Literal['parsl-htex'] = 'parsl-htex'
+    htex: HTExConfig = Field()
+    app_cache: Optional[bool] = Field(  # noqa: UP007
+        True,
+        description='enable app caching',
+    )
+    retries: int = Field(
+        0,
+        description='number of task retries in case of task failure',
+    )
+    strategy: Optional[str] = Field(  # noqa: UP007
+        'simple',
+        description='block scaling strategy',
+    )
+    max_idletime: Optional[float] = Field(  # noqa: UP007
+        120.0,
+        description='max idle time before strategy can shutdown unused blocks',
+    )
+    run_dir: str = Field(
+        'parsl-runinfo',
+        description='parsl run directory within the app run directory',
+    )
+
+    def get_executor(self) -> ParslPoolExecutor:
+        """Create an executor instance from the config."""
+        config = Config(
+            executors=[self.htex.get_executor()],
+            app_cache=self.app_cache,
+            retries=self.retries,
+            strategy=self.strategy,
+            max_idletime=self.max_idletime,
+            run_dir=self.run_dir,
+            initialize_logging=False,
+        )
+        return ParslPoolExecutor(config)
+
+
+class HTExConfig(BaseModel):
+    """Configuration for Parl's [`parsl.executors.HighThroughputExecutor`][parsl.executors.HighThroughputExecutor].
+
+    Note:
+        Optional attributes will default to Parsl's default values.
+
+    Note:
+        Extra options passed to this model will be provided as keyword
+        arguments to
+        [`parsl.executors.HighThroughputExecutor`][parsl.executors.HighThroughputExecutor].
+
+    Attributes:
+        provider: Configuration for the compute resource provider.
+        address: Address to connect to the main Parsl process.
+        worker_ports: Ports used by workers to connect to Parsl.
+        worker_port_range: Range of ports to choose worker ports from.
+        interchange_port_range: Ports used by Parsl to connect to the
+            interchange.
+    """  # noqa: E501
+
+    model_config = ConfigDict(extra='allow')
+
+    provider: Optional[ProviderConfig] = Field(  # noqa: UP007
+        None,
+        description='configuration for the compute resource provider',
+    )
+    address: Optional[Union[str, AddressConfig]] = Field(  # noqa: UP007
+        None,
+        description='address to connect to the main parsl process',
+    )
+    worker_ports: Optional[Tuple[int, int]] = Field(  # noqa: UP006,UP007
+        None,
+        description='ports used by workers to connect to parsl',
+    )
+    worker_port_range: Optional[Tuple[int, int]] = Field(  # noqa: UP006,UP007
+        None,
+        description='range of ports to choose worker ports from',
+    )
+    interchange_port_range: Optional[Tuple[int, int]] = Field(  # noqa: UP006,UP007
+        None,
+        description='ports used by parsl to connect to interchange',
+    )
+
+    def get_executor(self) -> HighThroughputExecutor:
+        """Create an executor instance from the config."""
+        options = self.model_dump(exclude_none=True)
+        if self.model_extra is not None:  # pragma: no branch
+            options.update(self.model_extra)
+
+        if self.provider is not None:
+            options['provider'] = self.provider.get_provider()
+        if self.address is not None and isinstance(
+            self.address,
+            AddressConfig,
+        ):
+            options['address'] = self.address.get_address()
+
+        return HighThroughputExecutor(label='taps-htex', **options)
+
+
+class AddressConfig(BaseModel):
+    """Parsl address configuration.
+
+    Example:
+        ```python
+        from parsl.addresses import address_by_interface
+        from taps.executor.config import AddressConfig
+
+        config = AddressConfig(kind='address_by_interface', ifname='bond0')
+        assert config.get_address() == address_by_interface(ifname='bond0')
+        ```
+    """
+
+    model_config = ConfigDict(extra='allow')
+
+    kind: str = Field(description='address function')
+
+    @field_validator('kind')
+    @classmethod
+    def _validate_address_name(cls, kind: str) -> str:
+        # Parse the class name if the full path is passed. For example,
+        # parsl.addresses.address_by_hostname and address_by_hostname should
+        # both be valid.
+        cls_name = kind.split('.')[-1]
+        try:
+            getattr(parsl.addresses, cls_name)
+        except AttributeError as e:
+            raise ValueError(
+                'The module parsl.addresses does not contain a provider '
+                f'named {cls_name}.',
+            ) from e
+
+        return cls_name
+
+    def get_address(self) -> str:
+        """Get the address according to the configuration."""
+        address_fn = getattr(parsl.addresses, self.kind)
+        options = self.model_extra if self.model_extra is not None else {}
+        return address_fn(**options)
+
+
+class ProviderConfig(BaseModel):
+    """Parsl execution provider configuration.
+
+    Example:
+        Create a provider configuration and call
+        [`get_provider()`][taps.executor.parsl.ProviderConfig.get_provider].
+        ```python
+        from taps.executor.config import ProviderConfig
+
+        config = ProviderConfig(
+            kind='PBSProProvider',
+            account='my-account',
+            cpus_per_node=32,
+            init_blocks=1,
+            max_blocks=1,
+            min_blocks=0,
+            nodes_per_block=1,
+            queue='debug',
+            select_options='ngpus=4',
+            walltime='00:30:00',
+            worker_init='module load conda',
+        )
+        config.get_provider()
+        ```
+        The resulting provider is equivalent to creating it manually.
+        ```python
+        from parsl.providers import PBSProProvider
+
+        PBSProProvider(
+            account='my-account',
+            cpus_per_node=32,
+            init_blocks=1,
+            max_blocks=1,
+            min_blocks=0,
+            nodes_per_block=1,
+            queue='debug',
+            select_options='ngpus=4',
+            walltime='00:30:00',
+            worker_init='module load conda',
+        ),
+        ```
+    """
+
+    model_config = ConfigDict(extra='allow')
+
+    kind: str = Field(description='name of execution provider')
+    launcher: Optional[LauncherConfig] = Field(  # noqa: UP007
+        None,
+        description='provider launcher',
+    )
+
+    @field_validator('kind')
+    @classmethod
+    def _validate_provider_name(cls, kind: str) -> str:
+        # Parse the class name if the full path is passed. For example,
+        # parsl.providers.SlurmProvider and SlurmProvider should both be valid.
+        cls_name = kind.split('.')[-1]
+        try:
+            getattr(parsl.providers, cls_name)
+        except AttributeError as e:
+            raise ValueError(
+                'The module parsl.providers does not contain a provider '
+                f'named {cls_name}.',
+            ) from e
+
+        return cls_name
+
+    def get_provider(self) -> ExecutionProvider:
+        """Create a provider from the configuration."""
+        options = self.model_extra if self.model_extra is not None else {}
+
+        if self.launcher is not None:
+            options['launcher'] = self.launcher.get_launcher()
+
+        provider_cls = getattr(parsl.providers, self.kind)
+        return provider_cls(**options)
+
+
+class LauncherConfig(BaseModel):
+    """Parsl launcher configuration.
+
+    Example:
+        Create a launcher configuration and call
+        [`get_launcher()`][taps.executor.parsl.LauncherConfig.get_launcher].
+        ```python
+        from taps.executor.config import LauncherConfig
+
+        config = LauncherConfig(
+            kind='MpiExecLauncher',
+            bind_cmd='--cpu-bind',
+            overrides='--depth=64 --ppn=1,
+        )
+        config.get_launcher()
+        ```
+        The resulting launcher is equivalent to creating it manually.
+        ```python
+        from parsl.launchers import MpiExecLauncher
+
+        MpiExecLauncher(bind_cmd='--cpu-bind', overrides='--depth=64 --ppn 1')
+        ```
+    """
+
+    model_config = ConfigDict(extra='allow')
+
+    kind: str = Field(description='name of provider launcher')
+
+    @field_validator('kind')
+    @classmethod
+    def _validate_launcher_name(cls, kind: str) -> str:
+        # Parse the class name if the full path is passed. For example,
+        # parsl.launchers.SrunLauncher and SrunLauncher should both be valid.
+        cls_name = kind.split('.')[-1]
+        try:
+            getattr(parsl.launchers, cls_name)
+        except AttributeError as e:
+            raise ValueError(
+                'The module parsl.launchers does not contain a provider '
+                f'named {cls_name}.',
+            ) from e
+
+        return cls_name
+
+    def get_launcher(self) -> Launcher:
+        """Create a launcher from the configuration."""
+        launcher_cls = getattr(parsl.launchers, self.kind)
+        options = self.model_extra if self.model_extra is not None else {}
+        return launcher_cls(**options)
--- tests/executor/parsl_test.py
+++ tests/executor/parsl_test.py
@@ -1,9 +1,23 @@
 from __future__ import annotations
 
 import pathlib
+import socket
 from concurrent.futures import Executor
+from unittest import mock
 
+import pytest
+from parsl.addresses import address_by_interface
+from parsl.executors import HighThroughputExecutor
+from parsl.launchers.base import Launcher
+from parsl.providers.base import ExecutionProvider
+from pydantic import ValidationError
+
+from taps.executor.parsl import AddressConfig
+from taps.executor.parsl import HTExConfig
+from taps.executor.parsl import LauncherConfig
+from taps.executor.parsl import ParslHTExConfig
 from taps.executor.parsl import ParslLocalConfig
+from taps.executor.parsl import ProviderConfig
 
 
 def test_get_local_executor(tmp_path: pathlib.Path) -> None:
@@ -11,3 +25,99 @@ def test_get_local_executor(tmp_path: pathlib.Path) -> None:
     config = ParslLocalConfig(run_dir=run_dir)
     executor = config.get_executor()
     assert isinstance(executor, Executor)
+
+
+def test_get_htex_executor(tmp_path: pathlib.Path) -> None:
+    htex_config = HTExConfig(
+        provider=ProviderConfig(
+            kind='PBSProProvider',
+            launcher=LauncherConfig(kind='SimpleLauncher'),
+            account='test-account',
+            cpus_per_node=32,
+            queue='debug',
+        ),
+        address=AddressConfig(kind='address_by_hostname'),
+        worker_ports=[0, 0],
+        worker_port_range=[0, 0],
+        interchange_port_range=[0, 0],
+    )
+
+    config = ParslHTExConfig(
+        htex=htex_config,
+        app_cache=False,
+        retries=1,
+        run_dir=str(tmp_path / 'parsl'),
+        max_workers_per_node=4,
+    )
+
+    with mock.patch.object(htex_config, 'get_executor'):
+        executor = config.get_executor()
+    assert isinstance(executor, Executor)
+
+
+def test_address_config() -> None:
+    ifname = socket.if_nameindex()[0][1]
+    config = AddressConfig(kind='address_by_interface', ifname=ifname)
+    assert config.get_address() == address_by_interface(ifname=ifname)
+
+
+def test_address_config_unknown_kind() -> None:
+    with pytest.raises(ValidationError, match='fake_address'):
+        AddressConfig(kind='fake_address')
+
+
+def test_launcher_config() -> None:
+    config = LauncherConfig(
+        kind='MpiExecLauncher',
+        bind_cmd='--cpu-bind',
+        overrides='--depth=64 --ppn=1',
+    )
+    assert isinstance(config.get_launcher(), Launcher)
+
+
+def test_launcher_config_unknown_kind() -> None:
+    with pytest.raises(ValidationError, match='FakeLauncher'):
+        LauncherConfig(kind='FakeLauncher')
+
+
+def test_provider_config() -> None:
+    config = ProviderConfig(
+        kind='PBSProProvider',
+        launcher=LauncherConfig(kind='SimpleLauncher'),
+        account='test-account',
+        cpus_per_node=32,
+        queue='debug',
+    )
+    assert isinstance(config.get_provider(), ExecutionProvider)
+
+
+def test_provider_config_default() -> None:
+    config = ProviderConfig(kind='AdHocProvider')
+    assert isinstance(config.get_provider(), ExecutionProvider)
+
+
+def test_provider_config_unknown_kind() -> None:
+    with pytest.raises(ValidationError, match='FakeProvider'):
+        ProviderConfig(kind='FakeProvider')
+
+
+def test_htex_config() -> None:
+    config = HTExConfig(
+        provider=ProviderConfig(
+            kind='PBSProProvider',
+            launcher=LauncherConfig(kind='SimpleLauncher'),
+            account='test-account',
+            cpus_per_node=32,
+            queue='debug',
+        ),
+        address=AddressConfig(kind='address_by_hostname'),
+        worker_ports=[0, 0],
+        worker_port_range=[0, 0],
+        interchange_port_range=[0, 0],
+    )
+    assert isinstance(config.get_executor(), HighThroughputExecutor)
+
+
+def test_htex_config_default() -> None:
+    config = HTExConfig()
+    assert isinstance(config.get_executor(), HighThroughputExecutor)
