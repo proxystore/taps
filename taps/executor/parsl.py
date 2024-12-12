5fdf6f09149f0d819edd353068939b51b4441e25
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-12-12 11:01:59 -0600

    Remove local channels from Parsl config

--- taps/executor/parsl.py
+++ taps/executor/parsl.py
@@ -10,7 +10,6 @@ from typing import Union
 
 import parsl
 from parsl.addresses import address_by_hostname
-from parsl.channels import LocalChannel
 from parsl.concurrent import ParslPoolExecutor
 from parsl.config import Config
 from parsl.executors import HighThroughputExecutor
@@ -67,7 +66,6 @@ class ParslLocalConfig(TapsExecutorConfig):
             address=address_by_hostname(),
             cores_per_worker=1,
             provider=LocalProvider(
-                channel=LocalChannel(),
                 init_blocks=1,
                 max_blocks=1,
             ),
