d9727634d12593853181e028260e3471c70fea55
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-12-23 23:59:10 -0600

    Remove unused type ignores in mypy 1.14

--- taps/apps/failures/app.py
+++ taps/apps/failures/app.py
@@ -98,12 +98,12 @@ class _FailureInjectionEngine(Engine):
             return cast(Task[P, R], failure_task), failure_type
 
         if failure_type == FailureType.DEPENDENCY:
-            wrapped = functools.partial(  # type: ignore[call-arg]
+            wrapped = functools.partial(
                 _wrapper_dependency_failure,
                 task,
             )
         else:
-            wrapped = functools.partial(  # type: ignore[call-arg]
+            wrapped = functools.partial(
                 _wrapper_generic_failure,
                 task,
                 FAILURE_FUNCTIONS[failure_type],
